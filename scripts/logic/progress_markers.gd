36f6f927518f15200a96307e8d04ad89f4c67985
ajdlanuza <adamjdlanuza@gmail.com>
2026-08-30 12:36:20 +0800

    refactor: switch to progress_markers.gd

---
+++ scripts/logic/progress_markers.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var has_asked: Dictionary = {
+	"Coughing_Fits": false,
+	"Cafe_Date": false,
+	"Felicia_Morning": false,
+	"Homecooking": false,
+	"Drew_Relationship": false,
+	"Drew_Date": false,
+	"Night_Call": false,
+	"Gary_Interview": false,
+	"Parent_Colleague": false,
+	"Child_Colleague": false,
+}
+
+var has_found: Dictionary = {
+	"Vault_Documents": true,
+	"Planted_Documents": false,
+	"Journal": false,
+}
+
+var has_met: Dictionary = {
+	"Felicia": false,
+	"Arthur": false,
+	"Gary": false,
+	"Francesca": false,
+	"Remi": false,
+}
